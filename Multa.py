e5ad29b80d4e8997f1261ff46978de483af710e2
victor0710s <80627658+victor0710s@users.noreply.github.com>
2021-04-12 16:10:26 -0300

    Update Multa.py

--- Multa.py
+++ Multa.py
@@ -3,16 +3,18 @@
 # 4 reais por quilo a mais
 
 peso_peixe = float(input('Quantos quilos de peixe você está levando: '))
+excesso = peso_peixe - 50.0
 
 
 def multa(peso_peixe):
-    return (peso_peixe - 50.0) * 4.0
+    return (excesso) * 4.0
 
 
 if __name__ == '__main__':
     Pmax = multa(peso_peixe)
     if peso_peixe > 50.0:
-        print(f'!Limite de peso excedido!n\
-            Valor da multa por execesso de peso: R$ {Pmax}')
+        print(f'!Limite de peso excedido!Excesso de: {excesso}Kg')
+        print(f'Valor da multa por execesso de peso: R$ {Pmax}')
     else:
         print('O peso informado está dentro dos limites.')
+
